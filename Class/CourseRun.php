f707b8d87037a0fd092195c1a65d0966378283e9
JIN NINGXIAN <JINNINGXIAN@HOTMAIL.COM>
2021-10-08 01:59:37 +0800

    update partial code for class

--- Class/CourseRun.php
+++ Class/CourseRun.php
@@ -3,8 +3,36 @@ class CourseRun{
     private courseRunID;
     private startDate;
     private endDate;
+    private sessionList = [];
+    // materials ?? datatype how to store?
+
+
+    public function __construct(startDate, endDate){
+        $this->startDate = startDate;
+        $this->endDate = endDate;
+    }
+
+    public function getStartDate(){
+        return $this->startDate;
+    }
+
+    public function getEndDate(){
+        return $this->endDate;
+    }
+
+    public function getID(){
+        return $this->courseRunID;
+    }
+
+    public function createSession(sectionNum){
+        $this->sessionList.append(new Session(sectionNum));
+    }
     
-    
+    function uploadCourseRunMaterials(){
+        // See the materials 
+        
+    }
+
 }
 
 ?>
--- Class/Section.php
+++ Class/Section.php
@@ -1,7 +1,18 @@
 <?php 
 class Section{
-    private sectionID;
-    
+    private sectionNum;
+    private quizList = [];
+    function __construct(sectionNum){
+        $this->sectionNum = sectionNum;
+    }
+    function createQuiz(quizID, quizType, quizQuestion, quizAnswer){
+        $this->quizList.append(new Quiz(quizID, quizType, quizQuestion, quizAnswer));
+    }
+
+    function autoGetQuizResult(quizID, answer){
+        
+    }
+
 }
 
 ?>
