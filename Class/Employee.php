d51ba036c4825fb159251e5e53f3cf706c954559
kjsh1708 <90863419+kjsh1708@users.noreply.github.com>
2021-10-12 15:49:06 +0800

    Latest Employee Class

--- Class/Employee.php
+++ Class/Employee.php
@@ -1,18 +1,44 @@
 <?php 
 
 class Employee{
-    private $name;
-    private $empID;
-    private $empType;
+    private $First_Name;
+    private $Last_Name;
+    private $Employee_ID;
+    private $Employee_Type;
+    private $Employee_Password_Hash;
 
-    function __construct($name, $empID, $empType){
-        $this->name = $name;
-        $this->empID = $empID;
-        $this->empType = $empType;
+    function __construct($First_Name, $Last_Name,$Employee_ID, $Employee_Type,$Employee_Password_Hash){
+        $this->First_Name= $First_Name;
+        $this->Last_Name= $Last_Name;
+        $this->Employee_ID =$Employee_ID;
+        $this->Employee_Type=$Employee_Type;
+        $this->Employee_Password_Hash=$Employee_Password_Hash;
+
     }
-    function getEmpoyerType(){
-        return $this->empType;
+    public function getEmpType(){
+        return $this->Employee_Type;
     }
+
+    public function getEmpFirstName(){
+        return $this->First_Name;
+    }
+
+    public function getEmpLastName(){
+        return $this->Last_Name;
+    }
+
+    public function getEmpID(){
+        return $this->Employee_ID;
+    }
+
+    public function getPasswordHash(){
+        return $this->Employee_Password_Hash;
+    }
+
+    public function setPasswordHash($hashed){
+        $this->Employee_Password_Hash = $hashed;
+    }
+
 }
 
 ?>
