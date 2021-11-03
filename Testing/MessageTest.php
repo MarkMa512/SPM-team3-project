af79fc6376d3f43cf75c46581bbc0c8a5affc002
MarkMa512 <42110231+MarkMa512@users.noreply.github.com>
2021-11-03 10:11:51 +0800

    Combine MessageTDD and PostTDD to MessageTest and PostTest.

--- Testing/MessageTest.php
+++ Testing/MessageTest.php
@@ -2,12 +2,16 @@
 require __DIR__ . '/../Class/Employee.php';
 use PHPUnit\Framework\TestCase;
 
-Class EmployeeTest extends TestCase
+Class MessageTest extends TestCase
 {
-    public function testConstructGetParams(){
-        
+public function testConstructGetParams(){
+        $testMessage = new Message(1001, 0001, "Test Message Content", "2021-02-02 18:00:00");
+        $this -> assertEquals(1001, $testMessage->getSenderID());
+        $this -> assertEquals(0001, $testMessage->getRecieverID());
+        $this -> assertEquals("Test Content", $testMessage->getMessageContent());
+        $this -> assertEquals("2021-02-02 18:00:00", $testMessage->getSentDateTime());
+        $this -> assertEquals(FALSE, $testMessage->getReadStatus());
     }
-
 }
 
 ?>
