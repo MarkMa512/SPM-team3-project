1a53ab33fe1578ebbad83779f7fae2c5cc7b1c87
JIN NINGXIAN <JINNINGXIAN@HOTMAIL.COM>
2021-11-03 22:53:38 +0800

    msg

--- View/Trainer/getData/getManageMaterialData.php
+++ View/Trainer/getData/getManageMaterialData.php
@@ -3,9 +3,9 @@ require_once "../../../Class/autoload.php";
 session_start();
 if(isset($_GET)){
     // var_dump($_GET);
-    $courseDao = new CourseDao();
+    $trainerDAO = new TrainerDAO();
     // var_dump($courseRunDao->generateMaterials());
-    $postJSON = json_encode($courseDao->getSectionMaterialById($_SESSION['user']->getEmpID()));
+    $postJSON = json_encode($trainerDAO->getMaterialsById($_SESSION['user']->getEmpID()));
     // $postJSON = json_encode($_GET);
     // $postJSON = json_encode($courseRunDao->generateMaterials("SR101", 1));
     // var_dump($courseRunDao->generateMaterials("SR101", 1));
