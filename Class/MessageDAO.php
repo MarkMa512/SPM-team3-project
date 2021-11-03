1fd6b629a88c50e72ac3f5c9a3b6e93bc9174d2d
MarkMa512 <42110231+MarkMa512@users.noreply.github.com>
2021-11-03 11:45:35 +0800

    Updated Message DAO Class

--- Class/MessageDAO.php
+++ Class/MessageDAO.php
@@ -5,14 +5,59 @@ class MessageDAO{
     function displayConversation($senderID, $recieverID){
         // input: senderID and recieverID
         // output: a list of Message Objects as the conversation between given sender and reciever
-        $result = 0; 
+        $conn = new ConnectionManager(); 
+        $pdo = $conn->getConnection(); 
+        $sql = "SELECT * FROM MSG WHERE Sender_ID = :sender_id AND Reciever_ID = :reciever_id;"; 
+        $stmt = $pdo->prepare($sql); 
+
+        $stmt->bindParam(":sender_id", $senderID, PDO::PARAM_STR); 
+        $stmt->bindParam(":reciever_id", $recieverID, PDO::PARAM_STR); 
+        
+        $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+        $status = $stmt->execute(); 
+        if(!$status){
+            var_dump($stmt->errorinfo());
+            # output any error if database access has problem
+        }
+        while($row = $stmt->fetch()){
+            $result[] = new Message($row['Sender_ID'], $row['Reciever_ID'], $row['Message_Content'], $row['Sent_Date_Time'], $row['Read_Status']); 
+        }
+        $stmt->closeCursor();
+        $pdo = NULL; 
+        
         return $result; 
     }
 
     function newMessage($messageObject){
         // input: a message object 
         // output: true if the database insertion is successful
-        return 0; 
+        $conn = new ConnectionManager(); 
+        $pdo = $conn-> getConnection(); 
+        $sql = "INSERT INTO MSG (Sender_ID, Reciever_ID, Message_Content, Read_Status)
+                VALUES                 (:sender_id, :reciever_id, :message_content, :read_status);"; 
+        $stmt = $pdo->prepare($sql); 
+        
+        $senderID = $messageObject->getSenderID(); 
+        $recieverID = $messageObject->getRecieverID();
+        $messageContent = $messageObject->getMessageContent(); 
+        $readStatus = $messageObject->getReadStatus();  
+
+
+        $stmt->bindParam(":sender_id", $senderID, PDO::PARAM_STR); 
+        $stmt->bindParam(":reciever_id", $recieverID, PDO::PARAM_STR); 
+        $stmt->bindParam(":message_content", $messageContent, PDO::PARAM_STR); 
+        $stmt->bindParam(":read_status", $readStatus, PDO::PARAM_STR); 
+
+        $status = $stmt->execute(); 
+
+        if(!$status){
+            var_dump($stmt->errorinfo());
+            # output any error if database access has problem
+        }
+
+        $stmt->closeCursor(); 
+        $pdo = NULL; 
+        return $status; 
     }
 
     function readMessage($messageObject){
