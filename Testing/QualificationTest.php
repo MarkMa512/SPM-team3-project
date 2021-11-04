443af9de5ee4aa90e66344fa538a137975fe0534
yumiao100 <90785480+yumiao100@users.noreply.github.com>
2021-11-05 00:54:10 +0800

    Update QualificationTest.php

--- Testing/QualificationTest.php
+++ Testing/QualificationTest.php
@@ -11,7 +11,7 @@ Class QualificationTest extends TestCase
     public function testConstructGetParams(){
         $qualification = new Qualification(1001, 'PT101', "2021-11-02 10:00:00", "2021-11-02 10:00:00");
         $this -> assertEquals(1001, $qualification->getEmployeeID());
-        $this -> assertEquals(0001, $qualification->getCourseCode());
+        $this -> assertEquals('PT101', $qualification->getCourseCode());
         $this -> assertEquals("2021-11-02 10:00:00", $qualification->getQualificationDate());
         $this -> assertEquals("2021-11-02 10:00:00", $qualification->getBadgeAssignDate());
     }
