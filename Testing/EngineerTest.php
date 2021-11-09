530c1af7d6d658d28b3a679e8a9fc16e2d981804
JIN NINGXIAN <JINNINGXIAN@HOTMAIL.COM>
2021-11-09 12:40:47 +0800

    testing

--- Testing/EngineerTest.php
+++ Testing/EngineerTest.php
@@ -1,11 +1,19 @@
 <?php
-require __DIR__ . '/../Class/Employee.php';
+require __DIR__ . '/../Class/Engineer.php';
 use PHPUnit\Framework\TestCase;
 
-Class EmployeeTest extends TestCase
+Class EngineerTest extends TestCase
 {
     public function testConstructGetParams(){
-        
+
+        $testEngineer = new Engineer("TestFirstName", "TestLastName",9999, "E", "BADPassword", "I");
+
+        $this -> assertEquals("TestFirstName",$testEngineer->getEmpFirstName());
+        $this -> assertEquals("TestLastName", $testEngineer->getEmpLastName()); 
+        $this -> assertEquals(9999, $testEngineer->getEmpID()); 
+        $this -> assertEquals("E", $testEngineer->getEmpType());
+
+        $this -> assertEquals('I', $testEngineer->getEngineerType());
     }
 
 }
