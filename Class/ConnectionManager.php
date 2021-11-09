26f501aba09973fb531aa3e466a4ff31ceaf05e5
JIN NINGXIAN <JINNINGXIAN@HOTMAIL.COM>
2021-11-09 17:07:34 +0800

    update chatfunction

--- Class/ConnectionManager.php
+++ Class/ConnectionManager.php
@@ -7,9 +7,9 @@ class ConnectionManager {
         // fill up based on your own configurations
         // $servername = 'localhost';
 
-        $servername = "localhost";
-        $username = 'root';
-        $password = '';
+        $servername = "database-1.cjfoojqqvgrl.us-east-2.rds.amazonaws.com";
+        $username = 'admin';
+        $password = 'pass1234';
         $dbname = 'LMS_Database';
 
         // Create connection
